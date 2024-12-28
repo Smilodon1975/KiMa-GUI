a15c84d0398c67cac0adca5aa1867ec3a132becf
Smilodon1975 <152981230+Smilodon75@users.noreply.github.com>
2024-12-28 21:23:20 +0100

    Initial commit: Angular Website Design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProbandenComponent } from './probanden/probanden.component';
+import { KundenComponent } from './kunden/kunden.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'probanden', component: ProbandenComponent },
+    { path: 'kunden', component: KundenComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+  ];
---
+++ src/app/kunden/kunden.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kunden',
+  standalone: true,
+  imports: [],
+  templateUrl: './kunden.component.html',
+  styleUrl: './kunden.component.css'
+})
+export class KundenComponent {
+
+}
