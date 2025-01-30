ab45a040cac9e41fc2704d9eac0769cc2c407735
Smilodon1975 <152981230+Smilodon75@users.noreply.github.com>
2025-01-30 18:15:55 +0100

    Routing works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'KiMa_Gui';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProbandenComponent } from './probanden/probanden.component';
 import { KundenComponent } from './kunden/kunden.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-    { path: 'probanden', component: ProbandenComponent },
-    { path: 'kunden', component: KundenComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', component: HomeComponent },  // Startseite
+  { path: 'home', component: HomeComponent },  // Home-Route für redirectTo
+  { path: 'probanden', component: ProbandenComponent },
+  { path: 'kunden', component: KundenComponent },
   ];
