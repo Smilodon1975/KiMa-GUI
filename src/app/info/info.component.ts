f7bbee41d6a6f2b63e4de7aa1efa2409390cd8e8
Smilodon1975 <152981230+Smilodon75@users.noreply.github.com>
2025-04-15 21:10:18 +0200

    Texte Start und Info

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { FAQ, FAQService } from '../services/faq.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+
 
 @Component({
   selector: 'app-info',  
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgOptimizedImage],
 })
 export class InfoComponent implements OnInit {
   faqs: FAQ[] = [];
