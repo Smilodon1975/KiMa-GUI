b9d206f209983aecf0200a898b13657396e46aa0
Smilodon1975 <152981230+Smilodon75@users.noreply.github.com>
2025-05-16 11:22:34 +0200

    scrolling + DataConsent Kästchen

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
-
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
 if (environment.production) {
   console.log('🚀 Produktionsmodus aktiviert');
@@ -18,6 +19,13 @@ bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top'
+      })
+    ),
+
     ...(appConfig.providers || [])
   ]
 })
