f315113929316a162726d84960fea1944f215153
Andreas K. <156953666+Smilodon1975@users.noreply.github.com>
2025-07-01 19:22:37 +0200

    Header Platzierung von Link Dauer-Projekt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://kima-api-cfbfe5fudddze5c3.westeurope-01.azurewebsites.net/api',
-  automotiveProjectId:32
+  automotiveProjectId:1
 };
--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,5 +1,5 @@
 // src/environments/version.ts
 export const version = {
-    number: '2.6.4-dev'
+    number: '2.6.5-dev'
   };
   
