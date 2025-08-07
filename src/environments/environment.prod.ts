50210eaf4d3b15f5205aab25e63a483bcdcfd7f1
Andreas K. <156953666+Smilodon1975@users.noreply.github.com>
2025-08-07 23:55:45 +0200

    Automotive ID inserted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://kima-api-cfbfe5fudddze5c3.westeurope-01.azurewebsites.net/api',
-  automotiveProjectId:1
+  automotiveProjectId:5
 };
