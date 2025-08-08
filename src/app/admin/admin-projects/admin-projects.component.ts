02cca3291070f450b17948307bf8c32287e0e135
Andreas K. <156953666+Smilodon1975@users.noreply.github.com>
2025-08-08 14:11:55 +0200

    Beschreibungseditoe und Tabellarische Darstellung der Responses added

--- src/app/admin/admin-projects/admin-projects.component.ts
+++ src/app/admin/admin-projects/admin-projects.component.ts
@@ -44,23 +44,24 @@ export class AdminProjectsComponent implements OnInit, AfterViewInit {
   openedResponses = new Set<number>();
   projectSearch = '';
   filteredProjects: Project[] = [];
-  quillModules: QuillModule = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block'],
-      [{ header: 1 }, { header: 2 }],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }],
-      [{ indent: '-1' }, { indent: '+1' }],
-      [{ direction: 'rtl' }],
-      [{ size: ['small', false, 'large', 'huge'] }],
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ color: [] }, { background: [] }],
-      [{ font: [] }],
-      [{ align: [] }],
-      ['link', 'image', 'video'],
-      ['clean'] 
-    ]
+  
+  quillModules = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ header: 1 }, { header: 2 }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    [{ direction: 'rtl' }],
+    [{ size: ['small', false, 'large', 'huge'] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }],
+    [{ font: [] }],
+    [{ align: [] }],
+    ['link', 'image', 'video'],
+    ['clean']
+  ]
   };
 
   constructor(private projectService: ProjectService, private router: Router) {}
